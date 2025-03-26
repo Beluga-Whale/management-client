46e8e6fc0bc79c1802357926b911edb2dbe90510
NB-66-08-02\NB-66-08-02 <savaddee159@gmail.com>
2025-03-26 16:50:40 +0700

    feat: create menu filter

--- app/(task)/tasks/page.tsx
+++ app/(task)/tasks/page.tsx
@@ -1,5 +1,13 @@
+import FilterPriority from "@/components/FilterPriority";
 import WidgetsIcon from "@mui/icons-material/Widgets";
 const TaskPage = () => {
-  return <div className="flex-9 p-4 bg-red-100 ">TaskPage</div>;
+  return (
+    <main className="flex-9 p-4 bg-slate-300 rounded-tl-xl rounded-tr-xl ">
+      <section className="flex justify-between">
+        <h1>All Tasks</h1>
+        <FilterPriority />
+      </section>
+    </main>
+  );
 };
 export default TaskPage;
