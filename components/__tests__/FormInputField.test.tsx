64ee1d659440137b3e2d494b6c0acd06bc5a882b
NB-66-08-02\NB-66-08-02 <savaddee159@gmail.com>
2025-04-11 12:43:17 +0700

    feat: add jest test setup

---
+++ components/__tests__/FormInputField.test.tsx
@@ -0,0 +1,52 @@
+import { FormProvider, useForm } from "react-hook-form";
+import FormInputField from "../FormInput/FormInputField";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+type TestFormData = {
+  email: string;
+};
+
+const RenderFormInputField = () => {
+  const form = useForm<TestFormData>({
+    defaultValues: { email: "" },
+  });
+  return (
+    <FormProvider {...form}>
+      <FormInputField
+        control={form.control}
+        name="email"
+        label="Email"
+        type="email"
+        placeholder="you@example.com"
+        description="Please enter a valid email."
+      />
+    </FormProvider>
+  );
+};
+
+describe("FormInputField", () => {
+  beforeEach(() => {
+    render(<RenderFormInputField />);
+  });
+
+  it("should render input field with label, placeholder", () => {
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/you@example.com/i)).toBeInTheDocument();
+  });
+
+  it("should render description", () => {
+    expect(
+      screen.getByText(/Please enter a valid email./i)
+    ).toBeInTheDocument();
+  });
+
+  it("should input field ", async () => {
+    const textInput = screen.getByPlaceholderText(/you@example.com/i);
+
+    await userEvent.type(textInput, "test@gmail.com");
+
+    expect((textInput as HTMLInputElement).value).toBe("test@gmail.com");
+  });
+});
